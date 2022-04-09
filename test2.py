08d0c144e6dedd7f10ab9f4a48e8f27ca3335f70
Mr-Lee-oss <lhd0@naver.com>
2022-04-09 22:52:47 +0900

    2

--- test2.py
+++ test2.py
@@ -8,3 +8,25 @@
 # 6009번
 #  a = input()
 # print(a) # 변수설정과 프린트만 하면 되는 간단한 문제였음
+#6010번
+#a=input() # 변수를 입력받을 것인지
+#a = int(a) # a로 변수로 입력받으면 이렇게 int(a)를 해줌
+#print(a) # 프린트해줌
+#6011번
+#a = input() #변수 입력 받음
+#a = float(a) #실수 입력 float
+#print(a)
+#6012번
+#a = input() #2개의 변수를 입력을 받고 출력하는 것
+#b = input()
+#a = int(a) 
+#b = int(b)
+#print(a)
+#print(b)
+#6013번
+#a = input() #줄을 바꿔서 출력하라고 했으니 출력을 두번하면 되는건가?
+#b = input()
+#a = str(a) # 문자로 받는다고 하니 문자인 str()로 해주면 됨
+#b = str(b)
+#print(b)
+#print(a)
